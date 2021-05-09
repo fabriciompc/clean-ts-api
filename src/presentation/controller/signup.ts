79eebbce75dcf1a91b891e55b1396bdac2936df4
Fabricio Cardoso <fabricio.cardoso@vizir.com.br>
2021-05-09 16:15:22 -0300

    refactor: move serverError to HttpHelper

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,9 +1,9 @@
-import { ServerError } from './../errors/server-error'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { HttpResponse } from '../protocols/http'
 import { InvalidParamError } from './../errors/invalid-param-error'
+import { serverError } from './../helpers/http-helper'
 import { EmailValidator } from './../protocols/email-validator'
 import { HttpRequest } from './../protocols/http'
 
@@ -34,10 +34,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
