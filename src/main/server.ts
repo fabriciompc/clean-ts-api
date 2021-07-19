4f66dc60b507134cb66b0c4bae217af32231c63f
Fabricio Cardoso <fabricio.cardoso@vizir.com.br>
2021-07-19 19:04:24 -0300

    refactor: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,11 @@
-import app from './config/app'
+import { MongoHelper } from '../infrastructure/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-app.listen(5050, () => console.log('Server Running at http://127.0.0.1:5050'))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () =>
+      console.log(`Server Running at http://127.0.0.1:${env.port}`)
+    )
+  })
+  .catch(console.error)
